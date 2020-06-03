05f953ae34c90492c7ec2cd6a4091c68f4ee4acb
Alain Saint-Dic <alains214@gmail.com>
2020-06-03 03:43:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,6 @@ long_planeteer_calls(["two", "go", "industrious", "bop"])
 
 def find_the_cheese(array)
   cheeses = ["cheddar", "gouda", "camembert"]
-  array.find 
-  end
-     end
+  array.find {|food| food == cheeses}
+end
 find_the_cheese(["crackers", "gouda", "thyme"])
